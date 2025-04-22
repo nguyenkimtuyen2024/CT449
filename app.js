2842e42130697b68965c232f58c4f52fb067ff51
nguyenkimtuyen2024 <tuyenvt23v7x339@dttx.ctu.edu.vn>
2025-04-22 22:03:56 +0700

    Cai dat cac handler truy xuat CSDL

--- app.js
+++ app.js
@@ -4,22 +4,25 @@ const contactsRouter = require("./app/routes/contact.route");
 const app = express();
 const ApiError = require("./app/api-error");
 
-app.use((req, res, next) => {
-return next(new ApiError(404, "Resource not found"));
-});
 app.use(cors());
 app.use(express.json());
+
 app.use("/api/contacts", contactsRouter);
+
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book application." });
 });
 
 
-app.use((err, req, res, next) => {
-return res.status(err.statusCode || 500).json({
-message: err.message || "Internal Server Error",
-});
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
 });
 
+
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
+});
 
 module.exports = app;
